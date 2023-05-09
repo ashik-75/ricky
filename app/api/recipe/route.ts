ef91ccf7578733615068631c4b47b88c89417d83
Ashik <ashik@pop-os.localdomain>
2023-05-09 09:11:48 +0600

    revome kv route

--- app/api/recipe/route.ts
+++
@@ -1,9 +0,0 @@
-import kv from "@vercel/kv";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const recipes = await kv.lrange("recipes", 0, -1);
-  return NextResponse.json({
-    recipes,
-  });
-}
